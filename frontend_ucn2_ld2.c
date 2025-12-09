6f4e4876137e91279a223119512c2fa8f627f94d
Thomas Lindner <lindner@triumf.ca>
2025-12-09 12:19:18 -0800

    Adding a few more variables.

--- frontend_ucn2_ld2.c
+++ frontend_ucn2_ld2.c
@@ -77,7 +77,7 @@ by EPICS.
 
 /* device driver list */
 DEVICE_DRIVER epics_driver[] = {
-  {"UCN2EpLD2", epics_ca, 60, NULL},  /* disable CMD_SET_LABEL */
+  {"UCN2EpLD2", epics_ca, 65, NULL},  /* disable CMD_SET_LABEL */
   {""}
 };
 
@@ -165,11 +165,11 @@ INT frontend_loop()
       if (hWatch) {
 	/* Check if Epics alive */
 	size = sizeof(float);
-	db_get_data_index(hDB, hRespond, &cat, &size, 59, TID_FLOAT);
+	db_get_data_index(hDB, hRespond, &cat, &size, 64, TID_FLOAT);
 	//if (abs(cat - dog) > 10.f)
 	//cm_msg(MINFO,"feEpics","R/W Access to Epics is in jeopardy!");
 	
-	db_set_data_index(hDB, hWatch, &dog, sizeof(float), 59, TID_FLOAT);
+	db_set_data_index(hDB, hWatch, &dog, sizeof(float), 64, TID_FLOAT);
       }
       if (!((INT)++dog % 100)) dog = 0.f;
     }
